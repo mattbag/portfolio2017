aa20044dbb602c6ac660e60260219242ef455f77
matt <matt@atlasagency.com.au>
2017-11-16 11:56:19 +1100

    trash all

--- path---categories-c-1-d-2-hi-0-fjx-sgwkwq-8-yuk-4-i-2-5359d9332061a624f713.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([53412951845664],{382:function(t,a){t.exports={data:{contentfulCategory:{title:{title:"Angular"},icon:{file:{url:"//images.contentful.com/u3ganegr6z7z/3OjEO6Pfe0aac4suYW4Go0/041c99df9dc9c9734a179ef99bc003cd/angular.svg"}}}},pathContext:{id:"c1D2hi0fjxSgwkwq8Yuk4I2",_PARENT:"SOURCE"}}}});
-//# sourceMappingURL=path---categories-c-1-d-2-hi-0-fjx-sgwkwq-8-yuk-4-i-2-5359d9332061a624f713.js.map
--- path---categories-c-44-us-lv-4-f-hy-2-ke-mw-i-0-umyo-g-d477ce525ebc714c73e0.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x5dc3ca639190],{383:function(t,e){t.exports={data:{contentfulCategory:{title:{title:"React"},icon:{file:{url:"//images.contentful.com/u3ganegr6z7z/s37YN66FLEoAaws6IEICU/dc52d14cd4a281359820b1ee75d745b0/react_copy.svg"}}}},pathContext:{id:"c44UsLv4FHy2KEMwI0UmyoG",_PARENT:"SOURCE"}}}});
-//# sourceMappingURL=path---categories-c-44-us-lv-4-f-hy-2-ke-mw-i-0-umyo-g-d477ce525ebc714c73e0.js.map
