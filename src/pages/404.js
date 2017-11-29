bccc2a0d752ee300c03c58b8d673bd848a0009f0
matt <matt@atlasagency.com.au>
2017-11-29 11:20:39 +1100

    404 emoji

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,27 +1,28 @@
 import React from "react"
 import Link from "gatsby-link"
-// import Img from "gatsby-image"
+import Helmet from "react-helmet";
 import logo from './../components/bio/face.png'
 import Bio from "./../components/bio/Bio";
 
 class Page404 extends React.Component {
     render() {
         return (
-            <div style={{ padding: `1rem`,textAlign: `center` }}>
-                
-                <Link to="/" style={{textDecoration:`none`}}>
-                <img src={logo} alt="matt bagni logo" style={{maxWidth: `40vw`}}/>
-                </Link>
-                <h1 style={{
-                    color: `#fff`,
-                    textShadow: `rgb(46, 142, 222) 2px 2px`,
-                    fontSize: `30vw`,
-                    fontDisplay: `swap`,
-                    textAlign: `center`,
-                }}>
-                    404
+            <div style={{ padding: `1rem`, textAlign: `center` }}>
+                <Helmet title={ `4😱4 - Matt Bagni` } />
+
+                <Link to="/" style={{ textDecoration: `none` }}>
+                    <img src={logo} alt="matt bagni logo" style={{ maxWidth: `40vw` }} />
+                    <h1 style={{
+                        color: `#fff`,
+                        textShadow: `rgb(46, 142, 222) 2px 2px`,
+                        fontSize: `30vw`,
+                        fontDisplay: `swap`,
+                        textAlign: `center`,
+                    }}>
+                        404
               </h1>
-              <br /> <br /> <br /> <br />
+                </Link>
+                <br /> <br /> <br /> <br />
                 <Bio bio={false} social={true} />
             </div>
         )
